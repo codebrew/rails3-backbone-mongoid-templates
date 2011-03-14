b52bc2b203186a134deadc36242dc6bfec2716d6
Ryan Fitzgerald <ryan@gymlogr.com>
2011-03-13 21:22:34 -0400

    packaging assets rake task

--- recipes/jammit.rb
+++ recipes/jammit.rb
@@ -7,4 +7,5 @@ ask "setup jammit? (yes/no)" do
   gsub_file 'app/views/layouts/application.html.haml', "= javascript_include_tag :defaults", "= include_javascripts :common, :application", :verbose => false
   gsub_file 'app/views/layouts/application.html.haml', "= stylesheet_link_tag :all", "= include_stylesheets :common", :verbose => false
   
+  copy_file File.expand_path("../../templates/package_assets.rake", __FILE__), "lib/tasks/package_assets.rake"
 end
